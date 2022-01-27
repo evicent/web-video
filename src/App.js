7319ce15764d6c2a68d7b9e8abb74495f4b17e0c
dlopez <e-dlopezp@ripley.com>
2022-01-27 20:26:27 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+     <ReactPlayer
+        // url='https://www.youtube.com/watch?v=Zdc3shRvumk' dedes de youtube
+        url={require('./videos/gato.mp4')} // local
+        width="100%"
+        height="100%"
+        controls
+        volumen={0.8}
+        className="react-player"
+     />
     </div>
   );
 }
